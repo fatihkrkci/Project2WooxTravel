fb39408d86d55c19c6314a66573a39d60ad1a4b3
Fatih Kürekçi <fatihkr02@gmail.com>
2024-10-10 22:16:11 +0300

    completed admin layout & created CategoryController & added Admin Entity

--- Areas/Admin/Controllers/AdminLayoutController.cs
+++ Areas/Admin/Controllers/AdminLayoutController.cs
@@ -8,10 +8,34 @@ namespace Project2WooxTravel.Areas.Admin.Controllers
 {
     public class AdminLayoutController : Controller
     {
-        // GET: Admin/AdminLayout
         public ActionResult Index()
         {
             return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
         }
     }
 }
